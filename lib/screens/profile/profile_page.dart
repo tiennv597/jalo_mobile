5e295a426a4140b556b101c3c2c16ff1d6cd5811
tiennv <tiennv597@gmail.com>
2020-06-23 00:44:31 +0700

    coding profile page

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -29,8 +29,112 @@ class _ProfilePageState extends State<ProfilePage> {
         builder: (context, snapshot) {
           User user = snapshot.data;
           if (snapshot.hasData) {
-            return Text(
-              user.displayName,
+            return Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.blue,
+                  backgroundImage: NetworkImage(
+                      'https://images.pexels.com/photos/340152/pexels-photo-340152.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                ),
+                Container(
+                  height: 8,
+                ),
+                Text(
+                  user.displayName,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: 18,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            "100K",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Following",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 1,
+                        height: 20,
+                        color: Colors.grey,
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            "10K",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Fans",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 1,
+                        height: 20,
+                        color: Colors.grey,
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            "900",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Heats",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                // Container(
+                //   height: 18,
+                // ),
+                // MaterialButton(
+                //   color: Colors.redAccent,
+                //   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                //   shape: RoundedRectangleBorder(
+                //     borderRadius: BorderRadius.circular(5),
+                //   ),
+                //   child: Text(
+                //     "Follow",
+                //     style: TextStyle(fontSize: 20, color: Colors.white),
+                //   ),
+                //   onPressed: () {},
+                // ),
+                // Container(
+                //   height: 18,
+                // ),
+                // Text(
+                //   "I am lyrically criminal",
+                //   style: TextStyle(color: Colors.grey),
+                // ),
+                // Container(
+                //   height: 8,
+                // ),
+                // Text("SEE TRANSLATION"),
+                // Container(
+                //   height: 18,
+                // ),
+              ],
             );
           } else {
             return RaisedButton(
