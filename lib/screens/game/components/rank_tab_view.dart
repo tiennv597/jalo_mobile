f96c2c4d4d48c3fc1ccba86f6cda2ab433576a53
tiennv <tiennv597@gmail.com>
2020-06-27 09:11:18 +0700

    coding rank tab view

---
+++ lib/screens/game/components/rank_tab_view.dart
@@ -0,0 +1,87 @@
+import 'package:shinro_int2/models/category.dart';
+import 'package:flutter/material.dart';
+import 'package:shinro_int2/screens/game/components/rank_list.dart';
+import 'package:shinro_int2/screens/main/components/category_card.dart';
+
+
+class RankTabView extends StatelessWidget {
+  final List<Category> categories = [
+    Category(
+      Color(0xffFCE183),
+      Color(0xffF68D7F),
+      'Title1',
+      'assets/image.jpg',
+    ),
+    Category(
+      Color(0xffF749A2),
+      Color(0xffFF7375),
+      'Title2',
+      'assets/image.jpg',
+    ),
+    Category(
+      Color(0xff00E9DA),
+      Color(0xff5189EA),
+      'Title3',
+      'assets/image.jpg',
+    ),
+    Category(
+      Color(0xffAF2D68),
+      Color(0xff632376),
+      'Title4',
+      'assets/image.jpg',
+    ),
+    Category(
+      Color(0xff36E892),
+      Color(0xff33B2B9),
+      'Title5',
+      'assets/image.jpg',
+    ),
+    Category(
+      Color(0xffF123C4),
+      Color(0xff668CEA),
+      'Title6',
+      'assets/image.jpg',
+    ),
+  ];
+
+  final TabController tabController;
+
+  RankTabView({Key key, this.tabController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+        //physics: NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: <Widget>[
+          Column(children: <Widget>[Flexible(child: RankList())]),
+          Column(children: <Widget>[
+            SizedBox(
+              height: 16.0,
+            ),
+            Flexible(child: RankList())
+          ]),
+          Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.all(8.0),
+                    height: MediaQuery.of(context).size.height / 9,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categories.length,
+                        itemBuilder: (_, index) => CategoryCard(
+                              category: categories[index],
+                            ))),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Flexible(child: RankList()),
+              ],
+            ),
+          ),
+        ]);
+  }
+}
--- lib/screens/game/game_home_page.dart
+++ lib/screens/game/game_home_page.dart
@@ -6,6 +6,7 @@ import 'package:shinro_int2/constant/shared_preferences.dart'
     as SHARED_PREFERNCES;
 import 'package:shinro_int2/constant/app_colors.dart' as COLORS;
 
+import 'components/rank_tab_view.dart';
 import 'game_room_page.dart';
 
 class GameHomePage extends StatefulWidget {
@@ -13,13 +14,17 @@ class GameHomePage extends StatefulWidget {
   _GameHomePageState createState() => _GameHomePageState();
 }
 
-class _GameHomePageState extends State<GameHomePage> {
+class _GameHomePageState extends State<GameHomePage>
+    with TickerProviderStateMixin<GameHomePage> {
   SwiperController swiperController = SwiperController();
+  TabController tabController;
   bool logined;
   String displayName;
+
   @override
   void initState() {
     super.initState();
+    tabController = TabController(length: 3, vsync: this);
     checkLogin();
   }
 
@@ -35,182 +40,146 @@ class _GameHomePageState extends State<GameHomePage> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildTypeBtn(String title, String urlImage, LinearGradient lgColor) {
     var withS = MediaQuery.of(context).size.width / 2.5;
     var heightS = MediaQuery.of(context).size.height / 8;
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
+      ),
+      color: Colors.white,
+      onPressed: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (_) => GameRoomPage()));
+      },
+      textColor: Colors.white,
+      padding: const EdgeInsets.all(0.0),
+      child: Container(
+        width: withS,
+        height: heightS,
+        decoration: BoxDecoration(
+          gradient: lgColor,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+                width: withS / 4,
+                height: withS / 4,
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                        fit: BoxFit.fill, image: ExactAssetImage(urlImage)))),
+            Text(title),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var withS = MediaQuery.of(context).size.width;
+    var heightS = MediaQuery.of(context).size.height / 2.1;
+    tabController = TabController(length: 3, vsync: this);
+
+    Widget tabBar = TabBar(
+      tabs: [
+        Tab(text: 'Related'),
+        Tab(text: 'Hot'),
+        Tab(text: 'Explore'),
+      ],
+      labelStyle: TextStyle(fontSize: 16.0),
+      unselectedLabelStyle: TextStyle(
+        fontSize: 14.0,
+      ),
+      labelColor: COLORS.darkGrey,
+      unselectedLabelColor: Color.fromRGBO(0, 0, 0, 0.5),
+      isScrollable: true,
+      controller: tabController,
+      indicatorColor: COLORS.transparentPurple,
+    );
+
+    // Widget _buildRankTab() {
+    //   return;
+    // }
+
     return Scaffold(
       body: SafeArea(
-          top: true,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 8, left: 20, bottom: 0),
-                child: Text("Game"),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 0),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            color: Colors.white,
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (_) => GameRoomPage()));
-                            },
-                            textColor: Colors.white,
-                            padding: const EdgeInsets.all(0.0),
-                            child: Container(
-                              width: withS,
-                              height: heightS,
-                              decoration: const BoxDecoration(
-                                gradient: COLORS.colorOrange,
-                              ),
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                      width: withS / 4,
-                                      height: withS / 4,
-                                      decoration: new BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          image: new DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image: ExactAssetImage(
-                                                  'assets/ninja.png')))),
-                                  const Text('Competition'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            color: Colors.white,
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (_) => GameRoomPage()));
-                            },
-                            textColor: Colors.white,
-                            padding: const EdgeInsets.all(0.0),
-                            child: Container(
-                              width: withS,
-                              height: heightS,
-                              decoration: const BoxDecoration(
-                                gradient: COLORS.colorBlue,
-                              ),
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                      width: withS / 4,
-                                      height: withS / 4,
-                                      decoration: new BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          border: Border.all(
-                                            color: Colors.purple,
-                                            width: 1.0,
-                                          ),
-                                          image: new DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image: ExactAssetImage(
-                                                  'assets/benkyou.png')))),
-                                  const Text('JLPT'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+        top: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 8, left: 20, bottom: 0),
+              child: Text("Game"),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 0),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: _buildTypeBtn('Competition', 'assets/ninja.png',
+                            COLORS.colorOrange),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: _buildTypeBtn(
+                            'JLPT', 'assets/benkyou.png', COLORS.colorBlue),
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            color: Colors.white,
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (_) => GameRoomPage()));
-                            },
-                            textColor: Colors.white,
-                            padding: const EdgeInsets.all(0.0),
-                            child: Container(
-                              width: withS,
-                              height: heightS,
-                              decoration: const BoxDecoration(
-                                gradient: COLORS.colorGreen,
-                              ),
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                      width: withS / 4,
-                                      height: withS / 4,
-                                      decoration: new BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          image: new DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image: ExactAssetImage(
-                                                  'assets/gokaku.png')))),
-                                  Text('Practice'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            color: Colors.white,
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (_) => GameRoomPage()));
-                            },
-                            textColor: Colors.white,
-                            padding: const EdgeInsets.all(0.0),
-                            child: Container(
-                              width: withS,
-                              height: heightS,
-                              decoration: const BoxDecoration(
-                                gradient: COLORS.colorPurple,
-                              ),
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                      width: withS / 4,
-                                      height: withS / 4,
-                                      decoration: new BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          image: new DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image: ExactAssetImage(
-                                                  'assets/good.png')))),
-                                  const Text('Competition'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: _buildTypeBtn(
+                            'Practice', 'assets/gokaku.png', COLORS.colorGreen),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: _buildTypeBtn('Competition', 'assets/good.png',
+                            COLORS.colorPurple),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Container(
+                width: withS,
+                height: heightS,
+                color: Colors.red,
+                child: NestedScrollView(
+                  headerSliverBuilder:
+                      (BuildContext context, bool innerBoxIsScrolled) {
+                    // These are the slivers that show up in the "outer" scroll view.
+                    return <Widget>[
+                      SliverToBoxAdapter(
+                        child: tabBar,
+                      )
+                    ];
+                  },
+                  body: RankTabView(
+                    tabController: tabController,
+                  ),
+                ),
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
